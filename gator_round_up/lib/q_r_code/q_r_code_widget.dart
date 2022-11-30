45765cbe72777319d869546c28b3d95cd3405d08
VBoudreau55 <vancebou55@gmail.com>
2022-11-30 02:43:31 -0500

    Adding event list to qr maker

--- gator_round_up/lib/q_r_code/q_r_code_widget.dart
+++ gator_round_up/lib/q_r_code/q_r_code_widget.dart
@@ -1,5 +1,8 @@
+import '../backend/backend.dart';
 import '../flutter_flow/flutter_flow_theme.dart';
+import '../flutter_flow/flutter_flow_util.dart';
 import 'package:flutter/material.dart';
+import 'package:qr_flutter/qr_flutter.dart';
 
 class QRCodeWidget extends StatefulWidget {
   const QRCodeWidget({Key? key}) : super(key: key);
@@ -55,6 +58,105 @@ class _QRCodeWidgetState extends State<QRCodeWidget> {
                         useGoogleFonts: false,
                       ),
                 )),
+              ),
+              Column(
+                //Event List
+                mainAxisSize: MainAxisSize.max,
+                children: [
+                  Row(
+                    mainAxisSize: MainAxisSize.max,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      SelectionArea(
+                          child: Text(
+                        'Upcoming Events',
+                        style: FlutterFlowTheme.of(context).bodyText1,
+                      )),
+                    ],
+                  ),
+                  StreamBuilder<List<EventsRecord>>(
+                    stream: queryEventsRecord(
+                      queryBuilder: (eventsRecord) => eventsRecord
+                          .where('StartTime',
+                              isGreaterThanOrEqualTo: getCurrentTimestamp)
+                          .orderBy('StartTime'),
+                      limit: 20,
+                    ),
+                    builder: (context, snapshot) {
+                      // Customize what your widget looks like when it's loading.
+                      if (!snapshot.hasData) {
+                        return Center(
+                          child: SizedBox(
+                            width: 50,
+                            height: 50,
+                            child: CircularProgressIndicator(
+                              color: FlutterFlowTheme.of(context).primaryColor,
+                            ),
+                          ),
+                        );
+                      }
+                      List<EventsRecord> listViewEventsRecordList =
+                          snapshot.data!;
+                      return ListView.builder(
+                        padding: EdgeInsets.zero,
+                        shrinkWrap: true,
+                        scrollDirection: Axis.vertical,
+                        itemCount: listViewEventsRecordList.length,
+                        itemBuilder: (context, listViewIndex) {
+                          final listViewEventsRecord =
+                              listViewEventsRecordList[listViewIndex];
+                          return InkWell(
+                            onTap: () async {
+                              FirebaseFirestore.instance
+                                  .collection('Events')
+                                  .doc(listViewEventsRecordList[listViewIndex]
+                                      .uid)
+                                  .get()
+                                  .then((DocumentSnapshot documentSnapshot) {
+                                if (documentSnapshot.exists) {
+                                  QrImage(
+                                    data: 'This is a simple QR code',
+                                    version: QrVersions.auto,
+                                    size: 320,
+                                    gapless: false,
+                                  );
+                                } else {
+                                  throw ('Document does not exist on the database');
+                                }
+                              });
+                            },
+                            child: ListTile(
+                              title: Text(
+                                listViewEventsRecord.eventTitle!,
+                                style: FlutterFlowTheme.of(context)
+                                    .title1
+                                    .override(
+                                      fontFamily: 'Metropolis',
+                                      color: FlutterFlowTheme.of(context)
+                                          .primaryColor,
+                                      useGoogleFonts: false,
+                                    ),
+                              ),
+                              subtitle: Text(
+                                '${dateTimeFormat('jm', listViewEventsRecord.startTime)} ${dateTimeFormat('MMMEd', listViewEventsRecord.eventDate)}',
+                                style: FlutterFlowTheme.of(context)
+                                    .subtitle2
+                                    .override(
+                                      fontFamily: 'Metropolis',
+                                      color: FlutterFlowTheme.of(context)
+                                          .secondaryColor,
+                                      useGoogleFonts: false,
+                                    ),
+                              ),
+                              tileColor: Color(0xFFF5F5F5),
+                              dense: false,
+                            ),
+                          );
+                        },
+                      );
+                    },
+                  ),
+                ],
               ),
             ],
           ),
