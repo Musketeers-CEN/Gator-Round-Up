0d07d51351f4f8a7733caee405177d1a8b40ebf0
A.V <aor7zov@gmail.com>
2022-12-05 21:00:01 -0500

    QR scanner initial with bugs

--- gator_round_up/lib/q_r_code_scan/q_r_code_scan_widget.dart
+++ gator_round_up/lib/q_r_code_scan/q_r_code_scan_widget.dart
@@ -1,8 +1,9 @@
+import 'dart:html';
 import 'package:gator_round_up/index.dart';
-
 import '../flutter_flow/flutter_flow_theme.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_barcode_scanner/flutter_barcode_scanner.dart';
+import 'package:mobile_scanner/mobile_scanner.dart';
 
 class QRCodeScanWidget extends StatefulWidget {
   const QRCodeScanWidget({Key? key}) : super(key: key);
@@ -12,15 +13,32 @@ class QRCodeScanWidget extends StatefulWidget {
 }
 
 /* Get a drop down or eventlist working then when an individual is selected it 
-1. opens qr code scanner
+1. opens qr code scanner (DONE THAT)
 2. once scanned grab the data and send to the selected event into the users list
 if not scanned do nothing
 
 */
 
 class _QRCodeScanWidgetState extends State<QRCodeScanWidget> {
-  final scaffoldKey = GlobalKey<ScaffoldState>();
-
+  final scaffoldKey = GlobalKey<ScaffoldState>();      
+      
+ @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Mobile Scanner')),
+      body: MobileScanner(
+          allowDuplicates: false,
+          onDetect: (barcode, args) {
+            if (barcode.rawValue == null) {
+              debugPrint('Failed to scan Barcode');
+            } else {
+              final String code = barcode.rawValue!;
+              debugPrint('Barcode found! $code');
+            }
+          }),
+    );
+  }
+/*
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -53,5 +71,5 @@ class _QRCodeScanWidgetState extends State<QRCodeScanWidget> {
         ),
       ),
     );
-  }
+  }*/
 }
