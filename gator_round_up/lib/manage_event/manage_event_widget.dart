4c4b58990f0dc069dec8be545e36b4a7cc6f3870
ColbyJack1134 <aydencol@hotmail.com>
2022-12-06 12:38:04 -0500

    manage event

--- gator_round_up/lib/manage_event/manage_event_widget.dart
+++ gator_round_up/lib/manage_event/manage_event_widget.dart
@@ -10,32 +10,56 @@ import '../flutter_flow/flutter_flow_util.dart';
 import '../flutter_flow/flutter_flow_widgets.dart';
 import 'package:flutter/material.dart';
 
-String eventId = "";
+
+String eventTitle = "Loading...";
+
+
 
 class ManageEventWidget extends StatelessWidget {
   final String eventId;
   ManageEventWidget({Key? key, required this.eventId}) : super(key: key);
+  
 
-  final scaffoldKey = GlobalKey<ScaffoldState>();
-
+  final scaffoldKey = GlobalKey<ScaffoldState>(); 
   @override
   Widget build(BuildContext context) {
     List<dynamic> group = [];
 
+    Future<DocumentSnapshot>? asyncFunction(){
+      print("HERE2");
+      
+      if(eventId == ""){
+        return null;
+      }
+
+      Future<DocumentSnapshot> document = FirebaseFirestore.instance
+            .doc(eventId)
+            .get()
+            .then((DocumentSnapshot documentSnapshot) {
+          if (documentSnapshot.exists) {
+            return documentSnapshot;
+          } else {
+            throw ('Document does not exist on the database');
+          }
+        });
+      return document;
+    }
+
     //document is still not being found
 
-    Future<List<dynamic>> document = FirebaseFirestore.instance
+    /*Future<List<dynamic>> document = FirebaseFirestore.instance
         .doc(eventId)
         .get()
         .then((DocumentSnapshot documentSnapshot) {
       if (documentSnapshot.exists) {
+        print("HERE");
+        eventTitle = documentSnapshot.get("EventTitle");
+        print(eventTitle);
         return group = documentSnapshot.get("Users");
       } else {
         throw ('Document does not exist on the database');
       }
-    });
-
-    print(group);
+    });*/
 
     List<String> userNames = [];
     group.forEach(
@@ -55,56 +79,68 @@ class ManageEventWidget extends StatelessWidget {
 
     print(userNames);
 
-    return Scaffold(
-      key: scaffoldKey,
-      backgroundColor: FlutterFlowTheme.of(context).primaryBackground,
-      appBar: AppBar(
-        backgroundColor: FlutterFlowTheme.of(context).primaryColor,
-        automaticallyImplyLeading: true,
-        title: Text(
-          eventId,
-          style: FlutterFlowTheme.of(context).title2.override(
-                fontFamily: 'Metropolis',
-                color: Colors.white,
-                fontSize: 22,
-                useGoogleFonts: false,
+    return FutureBuilder<DocumentSnapshot>(
+      future: asyncFunction(),
+      builder: (context, AsyncSnapshot<DocumentSnapshot> snapshot) {
+        if (snapshot.hasData) {
+          return Scaffold(
+            key: scaffoldKey,
+            backgroundColor: FlutterFlowTheme.of(context).primaryBackground,
+            appBar: AppBar(
+              backgroundColor: FlutterFlowTheme.of(context).primaryColor,
+              automaticallyImplyLeading: true,
+              title: Text(
+                eventId,
+                style: FlutterFlowTheme.of(context).title2.override(
+                      fontFamily: 'Metropolis',
+                      color: Colors.white,
+                      fontSize: 22,
+                      useGoogleFonts: false,
+                    ),
               ),
-        ),
-        actions: [],
-        centerTitle: true,
-        elevation: 2,
-      ),
-      body: SafeArea(
-        child: GestureDetector(
-          onTap: () => FocusScope.of(context).unfocus(),
-          child: Column(
-            mainAxisSize: MainAxisSize.max,
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              Column(
-                //Event List
-                mainAxisSize: MainAxisSize.max,
-                children: [
-                  Text("Event Title:"),
-                  // ListView.separated(
-                  //   padding: const EdgeInsets.all(8),
-                  //   itemCount: userNames.length,
-                  //   itemBuilder: (BuildContext context, int index) {
-                  //     return Container(
-                  //       height: 50,
-                  //       child: Center(child: Text('Entry ${userNames[index]}')),
-                  //     );
-                  //   },
-                  //   separatorBuilder: (BuildContext context, int index) =>
-                  //       const Divider(),
-                  // ),
-                ],
+              actions: [],
+              centerTitle: true,
+              elevation: 2,
+            ),
+            body: SafeArea(
+              child: GestureDetector(
+                onTap: () => FocusScope.of(context).unfocus(),
+                child: Column(
+                  mainAxisSize: MainAxisSize.max,
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    Column(
+                      //Event List
+                      mainAxisSize: MainAxisSize.max,
+                      children: [
+                        Text("Event Title:"),
+                        Text(snapshot.data?.get("EventTitle")),
+                        /*ListView.separated(
+                          padding: const EdgeInsets.all(8),
+                          itemCount: userNames.length,
+                          itemBuilder: (BuildContext context, int index) {
+                            return Container(
+                              height: 50,
+                              child: Center(child: Text('Entry ${userNames[index]}')),
+                            );
+                          },
+                          separatorBuilder: (BuildContext context, int index) =>
+                              const Divider(),
+                        ),*/
+                      ],
+                    ),
+                  ],
+                ),
               ),
-            ],
-          ),
-        ),
-      ),
+            ),
+          );
+        } else {
+          return CircularProgressIndicator();
+        }
+      }
     );
+
+    
   }
 }
 
