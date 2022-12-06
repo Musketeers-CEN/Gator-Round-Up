8645f9ebcd58fb9b4319f544a32372c41bd08d36
A.V <aor7zov@gmail.com>
2022-12-05 22:21:02 -0500

    QR scanner (added functionality) still buggy

--- gator_round_up/lib/q_r_code_scan/q_r_code_scan_widget.dart
+++ gator_round_up/lib/q_r_code_scan/q_r_code_scan_widget.dart
@@ -12,31 +12,71 @@ class QRCodeScanWidget extends StatefulWidget {
   _QRCodeScanWidgetState createState() => _QRCodeScanWidgetState();
 }
 
-/* Get a drop down or eventlist working then when an individual is selected it 
+/* 
+Get a drop down or eventlist working then when an individual is selected it 
 1. opens qr code scanner (DONE THAT)
-2. once scanned grab the data and send to the selected event into the users list
-if not scanned do nothing
-
+2. once scanned grab the data and send to the selected event into the users list if not scanned do nothing
 */
 
 class _QRCodeScanWidgetState extends State<QRCodeScanWidget> {
   final scaffoldKey = GlobalKey<ScaffoldState>();      
       
- @override
+ MobileScannerController cameraController = MobileScannerController();
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Mobile Scanner')),
-      body: MobileScanner(
-          allowDuplicates: false,
-          onDetect: (barcode, args) {
-            if (barcode.rawValue == null) {
-              debugPrint('Failed to scan Barcode');
-            } else {
-              final String code = barcode.rawValue!;
-              debugPrint('Barcode found! $code');
-            }
-          }),
-    );
+        appBar: AppBar(
+          backgroundColor: FlutterFlowTheme.of(context).primaryColor,
+          title: const Text('Mobile Scanner'),
+          actions: [
+            IconButton(
+              color: Colors.white,
+              icon: ValueListenableBuilder(
+                valueListenable: cameraController.torchState,
+                builder: (context, state, child) {
+                  switch (state as TorchState) {
+                    case TorchState.off:
+                      return const Icon(Icons.flash_off, color: Colors.grey);
+                    case TorchState.on:
+                      return const Icon(Icons.flash_on, color: Colors.yellow);
+                  }
+                },
+              ),
+              iconSize: 32.0,
+              onPressed: () => cameraController.toggleTorch(),
+            ),
+            IconButton(
+              color: Colors.white,
+              icon: ValueListenableBuilder(
+                valueListenable: cameraController.cameraFacingState,
+                builder: (context, state, child) {
+                  switch (state as CameraFacing) {
+                    case CameraFacing.front:
+                      return const Icon(Icons.camera_front);
+                    case CameraFacing.back:
+                      return const Icon(Icons.camera_rear);
+                  }
+                },
+              ),
+              iconSize: 32.0,
+              onPressed: () => cameraController.switchCamera(),
+            ),
+          ],
+        ),
+        body: SafeArea(
+          child: MobileScanner(
+            allowDuplicates: false,
+            controller: cameraController,
+            onDetect: (barcode, args) {
+              if (barcode.rawValue == null) {
+                debugPrint('Failed to scan Barcode');
+              } else {
+                final String code = barcode.rawValue!;
+                debugPrint('Barcode found! $code');
+              }
+            }))            
+          );         
   }
 /*
   @override
@@ -66,7 +106,17 @@ class _QRCodeScanWidgetState extends State<QRCodeScanWidget> {
           child: Column(
             mainAxisSize: MainAxisSize.max,
             crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [QRCodeScanWidget()],
+            children: MobileScanner(
+            allowDuplicates: false,
+            controller: cameraController,
+            onDetect: (barcode, args) {
+              if (barcode.rawValue == null) {
+                debugPrint('Failed to scan Barcode');
+              } else {
+                final String code = barcode.rawValue!;
+                debugPrint('Barcode found! $code');
+              }
+            }); 
           ),
         ),
       ),
